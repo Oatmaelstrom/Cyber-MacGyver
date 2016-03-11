0e5e0578a608ffcb66252f34ecec30bb2a8c9fda
Oatmaelstrom <selkon.hunter@gmail.com>
2016-03-11 10:16:43 -0500

    Update Entity.h

--- Entity.h
+++ Entity.h
@@ -3,6 +3,8 @@ class Entity{         //a base class from which all game entities can inherit
 int xpos, ypos;
 
 public:
+Entity(int initX, int initY): xpos(initX), ypos(initY) { }
+
 //setters
 void setX(newX) { xpos = newX; }
 void setY(newY) { ypos = newY; }
